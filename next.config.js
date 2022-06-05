ebcd3d1b83ab43de93630247a990d240397ad9e5
Marvin Sextro <marvin.sextro@gmail.com>
2022-06-05 21:27:58 +0200

    Remove api

--- next.config.js
+++ next.config.js
@@ -2,12 +2,4 @@ const {i18n} = require('./next-i18next.config');
 
 module.exports = {
   i18n,
-  async rewrites() {
-    return [
-        {
-            source: '/pass/note',
-            destination: '/pass'
-        }
-    ];
-  }
 };
